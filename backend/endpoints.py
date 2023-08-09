6e9b5ec287d72a921f4410461b70c38a03178331
unknown <cksdyd8350@gmail.com>
2023-08-09 20:08:55 +0900

    Implemented system prompt in gpt query

--- backend/endpoints.py
+++ backend/endpoints.py
@@ -1,13 +1,20 @@
 # endpoints.py
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
+
 import openai
 import os
 import requests
 from queryConfig import GPT_API_KEY
+from queryConfig import test_data
+from datetime import datetime
+
+import json
+
 router = APIRouter()
 
 openai.api_key = GPT_API_KEY
+
 
 @router.get("/")
 def read_root():
@@ -24,31 +31,47 @@ def test(params: dict):
     print(params)
 
     return {"query": "query here",
-    "answer" : "answer here",
-    "method" : "post"
-    }
+            "answer": "answer here",
+            "method": "post"
+            }
+
 
 @router.post("/test/query")
-def generate_response(params:dict):
+def generate_response(params: dict):
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
-        {
-            "role": "system",
-            "content": "you have to answer everything in korean"
-        },
-        {
-            "role" : "user",
-            "content" : params['message']
-        }
+            {
+                "role": "system",
+                "content": "You have to answer in the following format : {message : string, restaurants:[]}." +
+                "And content of message must be korean." +
+                "In the restaurants array, there should be objects in list below." +
+                "In the restaurants array, ther should be additional information, which is the coordinate of the restaurant. You have to convert location into coordinate"
+            },
+            {
+                "role": "system",
+                "content": "List of data is as follows : " + test_data
+            },
+            {
+                "role": "system",
+                "content": "Time now is : " + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " so your answer must include open restaurants."
+            },
+            {
+                "role": "user",
+                "content": params['message']
+            }
         ],
         temperature=0.2
-        )
+    )
 
-    print(response.choices[0].message['content'])  # Print the generated response
-    return {'answer':response.choices[0].message['content']}
+    # Print the generated response
+    answer = json.loads(response.choices[0].message['content'])
+    print(answer['message'])
+    return {'answer': response.choices[0].message['content']}
 
 # for testing. ignore it
+
+
 @router.get("/test/query/asd")
 def test_response():
     print("hi")
@@ -56,16 +79,28 @@ def test_response():
         model="gpt-3.5-turbo",
         messages=[
             {
-            "role": "system",
-            "content": "you have to answer everything in korean"
+                "role": "system",
+                "content": "You have to answer in the following format : {message : string, restaurants:[]}." +
+                "And content of message must be korean." +
+                "In the restaurants array, there should be objects in list below." +
+                "In the restaurants array, ther should be additional information, which is the coordinate of the restaurant. You have to convert location into coordinate"
             },
             {
-            "role": "user",
-            "content": "tell me about keyboard"
+                "role": "system",
+                "content": "List of data is as follows : " + test_data
+            },
+            {
+                "role": "system",
+                "content": "Time now is : " + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " so your answer must include open restaurants."
+            },
+            {
+                "role": "user",
+                "content": "recommend me some japanese food."
             }
         ],
         temperature=0.2
     )
 
-    print(response.choices[0].message['content'])  # Print the generated response
+    # Print the generated response
+    print(response.choices[0].message['content'])
     return response.choices[0].message['content']
