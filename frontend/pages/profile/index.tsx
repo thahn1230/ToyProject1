1df63b52bc49c7838ad1a427fa63a2bed1479fb9
mnseok <min.seok@g.skku.edu>
2023-08-16 14:20:04 +0900

    디자인 변경

---
+++ frontend/pages/profile/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function ProfilePage() {
+  return (
+    <>
+      <Head>
+        <title>profile</title>
+      </Head>
+
+      <h1>Profile Page</h1>
+    </>
+  );
+}
